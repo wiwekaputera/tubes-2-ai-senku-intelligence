94f70f246d5add35f1ec8297483818c0e8763e05
susTuna <hidasetsuna@gmail.com>
2025-11-26 18:53:58 +0700

    chore: integrate pipeliningto main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 # Add src to path so we can import modules
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
-from src.preprocessing import run_pipeline
+from src.preprocessing import run_full_pipeline as run_pipeline
 from src.dtl_scratch import DecisionTreeScratch
 from src.linear_models import LogisticRegressionScratch, OneVsAllClassifier
 from src.svm_scratch import SVMScratch
@@ -14,8 +14,7 @@ def main():
     
     # 1. Verify Preprocessing
     print("\n[1] Preprocessing Module: OK")
-    # Uncomment to run the actual pipeline if data exists
-    # run_pipeline()
+    run_pipeline()
 
     # 2. Verify Models
     dt = DecisionTreeScratch()
